00fdada8ca1ffb8b07bb79e2a2dedd57e6dba340
Volb Martin Robert - EMPORIA <volb@Emporia.local>
2022-02-07 11:37:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/batteryhealthapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.batteryhealthapp;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+   private Context activityContext;
+
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+    }
+}
